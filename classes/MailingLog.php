7e51dba0395490fc3ad5953f0f741d6256581cc5
Olivier Piton <olivier@cblue.be>
2021-03-04 13:44:52 +0100

    #322595

--- classes/MailingLog.php
+++ classes/MailingLog.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Mailing Class
+ * MailingLog Class
  *
  * @package    mod_recalluser
  * @author     olivier@cblue.be
