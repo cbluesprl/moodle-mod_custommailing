69f7df2315096edf96ed654e2c03bff92d887490
Hannecart Loïc <lhannecart@cblue.be>
2022-01-06 16:13:33 +0100

    #395719 maj v1.0.3 to v1.1.0

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2021051001;
 $plugin->requires = 2020061500; // Requires 3.9
 $plugin->component = 'mod_custommailing';
-$plugin->release = 'v1.0.3';
+$plugin->release = 'v1.1.0';
 $plugin->maturity = MATURITY_STABLE;
