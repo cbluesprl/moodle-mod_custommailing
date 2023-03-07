404b4321eac96e67a7532af5018f8f1d80225f1a
Julien Bisiaux <jbisiaux@cblue.be>
2023-03-07 11:17:39 +0100

    CB-533133 : Fix problem when 'at course enrollment'

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022051600;
+$plugin->version = 2023030700;
 $plugin->requires = 2020061500; // Requires 3.9
 $plugin->component = 'mod_custommailing';
-$plugin->release = 'v1.1.1';
+$plugin->release = 'v1.1.2';
 $plugin->maturity = MATURITY_STABLE;
